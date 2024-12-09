4ee03093d704236f2f6337667cf738ad7b20687b
Yasin <yasinisworking@gmail.com>
2024-12-09 19:58:51 +0330

    Bug fix attempt

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/E-commerceApp/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     vue(),
     svgLoader(),
